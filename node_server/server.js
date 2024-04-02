d180a80e878fb209a90735bc9a2ea0c48a4c19ad
ChuaJoshua <joshuachuamail@gmail.com>
2024-04-03 00:19:43 +0800

    Added Node Server
    IN PROGRESS: Socket Programming

---
+++ node_server/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const fs = require('fs');
+const path = require('path');
+
+// Define the directory where game records will be stored
+const recordsDirectory = path.join(__dirname, 'records');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = 5000;
+const HOST = '0.0.0.0';
+const gameSessions = new Map();
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+  
+    const sessionId = socket.handshake.query.sessionId;
+
+    // Join the specified room or channel
+    socket.join(sessionId);
+
+    if (!gameSessions.has(sessionId)) {
+        //Initailize a new game session
+        gameSessions.set(sessionId,  {board:[]});
+
+        //Add Text file for persistent storage
+        const filePath = path.join(recordsDirectory, `${sessionId}.txt`);
+        fs.writeFileSync(filePath, `Game session ID: ${sessionId}\n\n`);
+    }
+
+    // Handle disconnections
+    socket.on('disconnect', () => {
+  
+    });
+
+    // Handle updates
+    socket.on('update', (updatedBoard, player) => {
+        // Update the board in the gameSessions map
+        gameSessions.set(sessionId, { board: updatedBoard });
+
+        // Broadcast the updated board to all clients in the session
+        io.to(sessionId).emit('update', updatedBoard);
+    });
+    
+});
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}/`);
+  });
