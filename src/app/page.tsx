f8c8cdd059197853c1365cedac9e4b9e7322a70f
ChuaJoshua <joshuachuamail@gmail.com>
2024-04-06 23:28:27 +0800

    Added Lobby Screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
+
+  const [userInput, setUserInput] = useState("");
+  const handleJoinSession = () => {
+    if (!isNaN(parseInt(userInput))) {
+      window.location.href = "/tic-tac-toe/" + userInput;
+    } else {
+      alert("Invalid input");
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <div className="bg-white flex items-center justify-center lg:pt-5 w-full min-h-screen">
+      <div className="flex flex-col px-6 py-12 bg-white lg:w-1/3 lg:border lg:shadow-md h-fit">
+        <div className="text-primary text-4xl font-bold font-serif text-center">
+          Tic Tac Toe
+        </div>
+        <div className="text-slate-950 text-lg text-center p-1">
+          Enter a Room Number to Join a Session
+        </div>
+        <input
+          type="Number"
+          name="Room Number"
+          placeholder="Room Number"
+          className="border-b-2 border-primary p-2 m-2"
+          value={userInput}
+          onChange={(input) => setUserInput(input.target.value)}
+        ></input>
         
+        <div className="bg-black text-white  font-bold p-2 m-2 rounded-lg" onClick={handleJoinSession}>
+          Click here to Join Session
+        </div>
+
       </div>
-    </main>
+    </div>
   );
 }
--- src/app/tic-tac-toe/[id]/page.tsx
+++ src/app/tic-tac-toe/[id]/page.tsx
@@ -123,11 +123,11 @@ export default function Game({ params }: { params: { id: string } }) {
 
     if (squares == null) {
         return (
-            <div className="bg-white lg:flex lg:justify-center lg:pt-5 w-full min-h-screen items-center">
-                <div className="flex flex-col px-6 py-12 bg-white items-center">
-                    <h1 className="text-3xl font-bold pb-10">Loading ....</h1>
-                </div>
+        <div className="bg-white lg:flex lg:justify-center lg:pt-5 w-full min-h-screen items-center">
+            <div className="flex flex-col px-6 py-12 bg-white items-center">
+                <h1 className="text-3xl font-bold pb-10">Loading ...</h1>
             </div>
+        </div>
         )
     }
 
